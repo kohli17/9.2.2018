accbf8008f247ef178ba201d75e298cd473a7f37
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-09 16:33:42 +0530

    Create SUM.C
    add two number

---
+++ SUM.C
@@ -0,0 +1,16 @@
+include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+	int n,sum=0,rem;
+	cin>>n;
+	while(n>0)
+	{
+	    rem=n%10;
+	    sum=sum+rem;
+	    n/=10;
+	}
+	cout<<sum;
+	return 0;
+}
